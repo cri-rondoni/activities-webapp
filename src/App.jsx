b442693a9c0e5a25256141454f7b0a11711f7c0f
cri-rondoni <cristiana.rondoni@unicampus.it>
2025-10-02 03:58:34 +0000

    Aggiunto trova le differrenze

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Walk from "./components/Walk";
-import Breath from "./components/Breath";
-import Stretch from "./components/Stretch";  // 👈 nuovo import
+import SpotDiff from "./components/SpotDiff";
 
 export default function App() {
   return (
@@ -10,7 +6,8 @@ export default function App() {
       <Routes>
         <Route path="/walk" element={<Walk />} />
         <Route path="/breath" element={<Breath />} />
-        <Route path="/stretch" element={<Stretch />} /> {/* 👈 nuova route */}
+        <Route path="/stretch" element={<Stretch />} />
+        <Route path="/spotdiff" element={<SpotDiff />} /> {/* 👈 nuova route */}
       </Routes>
     </BrowserRouter>
   );
